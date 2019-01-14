0e0bd2fcf45dfdc67d14d56890fa7905e2afc156
Muhammed Aljailane <darhost56@gmail.com>
2019-01-14 18:32:45 +0300

    Update head.php
    تعديل الهيدر

--- inc/head.php
+++ inc/head.php
@@ -6,9 +6,10 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />  
          <!-- Bootstrap CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-     <script src="/ckeditor/ckeditor.js"></script>
-<script>
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+         <script src="/ckeditor/ckeditor.js"></script>
+
+            <script>
 $('.btn').on('click', function() {
     var $this = $(this);
   $this.button('loading');
